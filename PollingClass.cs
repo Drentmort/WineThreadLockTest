c82a1c272a6f27e9196a8698d3f58bb5ca98ffb0
Anton Andryakov <60717352+Drentmort@users.noreply.github.com>
2023-05-02 17:46:17 +0300

    Add files via upload

---
+++ PollingClass.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerConsoleTest
+{
+    internal class PollingClass
+    {
+        private  readonly object _o = new object();
+        private readonly PollerTimer _timer = new PollerTimer(500);
+        public event Action<TimeSpan> TimeLockHandler;
+
+        public void Start()
+        {
+            _timer.NeedPoll += () =>
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+                Monitor.Enter(_o);
+                Monitor.Exit(_o);
+                timer.Stop();
+
+                TimeLockHandler?.Invoke(timer.Elapsed);
+            };
+            _timer.Start(); 
+        }
+        public void Stop() => _timer.Stop();
+    }
+
+    public class PollerTimer
+    {
+
+        private const int PollingPeriodInMs = 500;
+        private readonly Timer _pollerTimer;
+        private readonly object _timerLocker = new object();
+        private bool _isDisposed;
+        private int _pollingPeriodInMs;
+
+        public event Action NeedPoll = delegate { };
+
+        public PollerTimer() : this(PollingPeriodInMs)
+        {
+
+        }
+
+        public PollerTimer(int periodMs)
+        {
+            _pollingPeriodInMs = periodMs;
+            _pollerTimer = new Timer(OnTimerTick, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public void Start()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+            _pollerTimer.Change(_pollingPeriodInMs, _pollingPeriodInMs);
+        }
+
+        public void Start(int periodMs)
+        {
+            ChangePeriod(periodMs);
+            Start();
+        }
+
+        public void Stop()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+            _pollerTimer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public void Dispose()
+        {
+            _isDisposed = true;
+            _pollerTimer.Dispose();
+        }
+
+
+        private void OnTimerTick(object state)
+        {
+            Stop();
+            try
+            {
+                NeedPoll();
+            }
+            catch (Exception e)
+            {
+
+            }
+            Start();
+        }
+
+        public void ChangePeriod(int periodMs)
+        {
+            _pollingPeriodInMs = periodMs;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerConsoleTest
+{
+    internal class Program
+    {
+        static ManualResetEvent @event = new ManualResetEvent(false);
+
+        static double summ = 0;
+        static double count = 0;
+        static object _o = new object();
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число таймеров");
+
+            var timerCountStr = Console.ReadLine();
+            if (!int.TryParse(timerCountStr, out var timerCount))
+            {
+                timerCount = 100;
+            }
+
+            Console.WriteLine($"Число таймеров {timerCount}");
+
+            var tList =new List<PollingClass>();
+            for(int i = 0; i < timerCount; i++)
+            {
+                var t = new PollingClass();
+                t.TimeLockHandler += time =>
+                {
+                    lock (_o)
+                    {
+                        Console.WriteLine($"{DateTime.Now} очередной вызов занял {time.TotalMilliseconds} миллисекунд");
+                        summ += time.TotalMilliseconds;
+                        count++;
+                        Console.WriteLine($"{DateTime.Now} в среденем {summ / count} миллисекунд");
+                    }
+                    
+                };
+                tList.Add(t);
+                t.Start();
+                
+            }           
+            @event.WaitOne();
+        }
+    }
+
+    
+}
